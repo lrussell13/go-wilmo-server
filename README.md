d96994faaa56a39b62450fe6d83a036851242b69
Lance Russell <lancebryanrussell@gmail.com>
2020-05-11 18:49:35 -0400

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wellread
+
+Live App: https://wellread.lancebryanrussell.now.sh/
+Client Repo: https://github.com/lrussell13/wellread
+
+![screenshots](src/images/readme-image.png)
+
+## About
+
+Wellread is an app that helps readers feel like they are upgrading their lives with every finished book. Users can track their reading habits and feel rewarded by progressing through a level based system. 
+
+## Technologies Used:
+* React
+* CSS
+* Node
+* Express
+* PostgreSQL
+* Api:
+    * OpenLibrary
+
---
+++ src/lvl/level-router.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const levelService = require('./level-service');
+const { requireAuth } = require('../middleware/jwt-auth')
+
+const lvlRouter = express.Router();
+const bodyParser = express.json();
+
+
+lvlRouter
+    .route('/')
+    .get(requireAuth, (req, res) => {
+        const knex = req.app.get('db');
+        levelService.getUserLvlByUserId(knex, req.user.id)
+        .then(lvl => {
+            res.json(lvl);
+        });
+    })
+    .post(bodyParser, (req, res, next) => {
+        const knex = req.app.get('db');
+        const { user_id } = req.body;
+        const newUserLvl= { user_id };
+
+        for (const [key, value] of Object.entries(newUserLvl))
+            if (value == null){
+                return res.status(400).json({
+                error: { message: `Missing '${key}' in request body` }
+            })}
+
+        levelService.insertUserLvl(knex, newUserLvl)
+        .then(() => {
+            res.status(201).end();
+        })
+        .catch(next)
+    })
+
+lvlRouter
+    .route('/:id')
+    .all(requireAuth)
+    .delete((req, res, next) => {
+        levelService.deleteUserBook(req.app.get('db'), req.params.id)
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+    .patch(bodyParser, (req, res, next) => {
+        const { lvl } = req.body;
+        const updatedUserLvl = { lvl };
+
+        if (!updatedUserLvl)
+            return res.status(400).json({
+            error: {
+                message: `Request body must contain lvl`
+            }
+        })
+
+        levelService.updateUserLvl(req.app.get('db'), updatedUserLvl, req.params.id)
+        .then(numRowsAffected => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+
+module.exports = lvlRouter;
